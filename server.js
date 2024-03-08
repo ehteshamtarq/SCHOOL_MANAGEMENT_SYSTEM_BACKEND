1ffddd448a6ed52d26a704e6786ab82b867a350e
ehteshamtarq <ehteshamtarique1@gmail.com>
2024-03-08 11:50:52 +0530

    Admin and Program Schema

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const app = require("./app/app");
+
+const app = express();
+
+const PORT = process.env.port || 5000
+
+
+// server
+
+const server = http.createServer(app)
+server.listen(PORT, console.log(`Server is running ${PORT}`));
